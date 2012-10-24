75d9de6b0209ac0c081c4c0c2ba94754f11d26ab
Brian Olore <brian@olore.net>
2012-10-23 23:02:56 -0400

    remove outdated JS files,added html5.js dependency

--- grunt.js
+++ grunt.js
@@ -1,12 +1,17 @@
 module.exports = function(grunt) {
 
+  grunt.loadNpmTasks('grunt-contrib'); //https://npmjs.org/package/grunt-contrib , $ npm install grunt-contrib-copy
+
+
   grunt.initConfig({
 
     concat: {
-
       dist: {
-        src: [  'dependencies/platformjs/platform.js',            'dependencies/benchmarkjs/benchmark.js', 
-                'dependencies/benchmarkjs/example/jsperf/ui.js',  'dependencies/benchmarkjs/plugin/ui.browserscope.js'],
+        src: [  'dependencies/platformjs/platform.js',            
+                'dependencies/benchmarkjs/benchmark.js', 
+                'dependencies/benchmarkjs/example/jsperf/ui.js',  
+                'dependencies/benchmarkjs/plugin/ui.browserscope.js'
+        ],
         dest: '_js/main.src.js'
       }
     },
@@ -16,13 +21,22 @@ module.exports = function(grunt) {
         src: ['_js/main.src.js'],
         dest: '_js/main.js'
       }
+    },
+
+    copy: {
+      dist: {
+        files: {
+          "_js/html5.js": "dependencies/html5js/html5.js"
+        }
+
+      }
     }
 
   });
 
 
   // Default task.
-  grunt.registerTask('default', 'concat min');
+  grunt.registerTask('default', 'concat copy min');
 
 
 };
